d367a9f77aa779ee90130bbb2a04eb0318030653
mitmirzutun <milan1111@gmx.de>
2024-03-05 23:54:49 +0100

    Added a python wrapper for PrimeGenerator

--- Rust_Cargo/milans_pyo3_library/src/lib.rs
+++ Rust_Cargo/milans_pyo3_library/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn main() {
     println!("Hello, world!");
 }
+#[pyo3::prelude::pymodule]
+fn milans_pyo3_library<'py>(
+    _py: pyo3::Python<'py>,
+    module: &'py pyo3::prelude::PyModule,
+) -> pyo3::PyResult<()> {
+    module.add_class::<PrimeGeneratorWrapper>()?;
+    Ok(())
+}
+#[pyo3::prelude::pyclass]
+#[pyo3(name = "PrimeGenerator")]
+pub struct PrimeGeneratorWrapper {
+    generator: milans_rust_core::math::primality::PrimeGenerator<u8>,
+}
