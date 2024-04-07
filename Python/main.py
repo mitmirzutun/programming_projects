50b78ce0f7d4b02e27c0ffa26fd6948732ad53d4
mitmirzutun <milan1111@gmx.de>
2024-04-07 12:49:24 +0200

    Formatted Code

--- Python/main.py
+++ Python/main.py
@@ -1,20 +1,21 @@
 import core
 import my_turtle
+import AI
 
 
 @core.build
 def main():
     import milans_pyo3_library
     # my_turtle.AnalogClock().run()
-    for i in range(1,128):
-        if not milans_pyo3_library.miller_rabin_test((1<<i)-1):
-            print(i,(1<<i)-1,milans_pyo3_library.prime_divisors((1<<i)-1))
+    for i in range(1, 16):
+        if not milans_pyo3_library.miller_rabin_test((1 << i)-1):
+            print(i, (1 << i)-1, milans_pyo3_library.prime_divisors((1 << i)-1))
         else:
-            print(i,(1<<i)-1)
-        if not milans_pyo3_library.miller_rabin_test((1<<i)+1):
-            print(i,(1<<i)+1,milans_pyo3_library.prime_divisors((1<<i)+1))
+            print(i, (1 << i)-1)
+        if not milans_pyo3_library.miller_rabin_test((1 << i)+1):
+            print(i, (1 << i)+1, milans_pyo3_library.prime_divisors((1 << i)+1))
         else:
-            print(i,(1<<i)+1)
+            print(i, (1 << i)+1)
 
 
 if __name__ == '__main__':
