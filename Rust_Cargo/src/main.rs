da472b0e70550ce780acc666e477c09ddd508d74
mitmirzutun <milan1111@gmx.de>
2024-04-24 20:38:36 +0200

    set up Cargo.toml & log4rs

--- Rust_Cargo/src/main.rs
+++ Rust_Cargo/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello, world!");
+    milans_rust_logging::configure("resources/log.yml").unwrap();
+    milans_rust_logging::configure("resources/log.yml").unwrap();
+    milans_rust_logging::error!("Hello world");
+    milans_rust_logging::warn!("Hello world");
+    milans_rust_logging::info!("Hello world");
+    milans_rust_logging::debug!("Hello world");
+    milans_rust_logging::trace!("Hello world");
 }
