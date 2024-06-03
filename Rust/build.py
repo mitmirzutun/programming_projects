ca206e7bdbda69246d3b300993411083eea59075
mitmirzutun <milan1111@gmx.de>
2024-06-03 21:31:57 +0200

    Ammended build.py

--- Rust/build.py
+++ Rust/build.py
@@ -9,14 +9,15 @@ def main():
     __path__=os.path.dirname(__file__)
     with open(os.path.join(__path__,"Cargo.toml"),"rb") as tomlfile:
         workspaces=list(tomllib.load(tomlfile)["workspace"]["members"])
-        workspaces.append(".")
         workspaces=list(map(os.path.abspath,workspaces))
     source_folders=list(map(lambda x: os.path.join(x,"src"),workspaces))
     print(source_folders)
     source_files=[__file__]
     for workspace in source_folders:
         source_files += list(get_all_files(workspace))
-    target_files=get_all_files(os.path.join(__path__,"target"))
+    result=subprocess.run(["ls","-a",os.path.join(__path__,"target","debug")],check=True,capture_output=True)
+    target_files=list(filter(os.path.isfile,map(lambda x: os.path.join(__path__,"target","debug",x.decode("utf-8")),result.stdout.split(b"\n"))))
+    print(target_files)
     if os.path.getmtime(max(source_files,key=os.path.getmtime))<os.path.getmtime(max(target_files,key=os.path.getmtime)):
         return
     #subprocess.run(["cp","target/debug/libmilans_pyo3_library.so","../Python/milans_pyo3_library.so"],cwd=__path__)
@@ -29,7 +30,8 @@ def main():
         try:
             for workspace in workspaces:
                 basedir=os.path.basename(workspace)
-                result=subprocess.run(["cargo","expand"], cwd=workspace, capture_output=True)
+                print(basedir)
+                result=subprocess.run(["cargo","expand","--lib"], cwd=workspace, capture_output=True)
                 with open(os.path.join(__path__,"expanded",f"{basedir}.rs"),"wb") as expanded:
                     expanded.write(result.stdout)
                 subprocess.run(["cargo","test"],cwd=workspace,check=True)
